ac2806aa1531dee5a7449e0b38c9cf8a90829abe
saurov-paul <saurov.paul1@gmail.com>
2024-03-31 15:35:43 +0300

    pancake

---
+++ PanCake/main.js
@@ -0,0 +1,76 @@
+const form = document.querySelector(".form-container");
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const typeSelect = document.querySelector("#type"); // <select>
+const orderButton = document.querySelector("#orderButton");
+
+let total;
+let toppings = [];
+let extras = [];
+
+const pancakePriceCalc = () => {
+  // total price icon
+  const totalPriceElement = document.querySelector("#totalPrice"); //span - total price
+  const priceBanner = document.querySelector(".price-banner");
+
+  total = parseInt(typeSelect.value);
+  totalPriceElement.textContent = `$${total}`;
+  checkToppings();
+
+  // Animation for price banner
+  priceBanner.animate(
+    [
+      { transform: "scale(1)" },
+      { transform: "scale(1.05)" },
+      { transform: " scale(1)" },
+    ],
+    {
+      duration: 100,
+      iterations: 1,
+    }
+  );
+};
+
+// Function to add an item to toppings or extras array
+const addItem = (itemName, category) => {
+  if (category === "toppings") {
+    toppings.push(itemName);
+  } else {
+    extras.push(itemName);
+  }
+};
+const removeItem = () => {};
+
+const checkToppings = () => {
+  toppings = [];
+  extras = [];
+
+  for (const item of checkboxes) {
+    const itemName = item.dataset.name;
+    const category = item.dataset.category;
+
+    if (item.checked) {
+      total += parseInt(item.value);
+      addItem(itemName, category);
+    } else {
+      removeItem(itemName, category);
+    }
+  }
+  console.log("toppings array", toppings);
+  console.log("extras array", extras);
+};
+const displayOrder = () => {
+  const customerName = document.querySelector("#customerName").value;
+  const orderType = document.querySelector("#order_type");
+  const orderToppings = document.querySelector("#order_toppings");
+  const orderExtras = document.querySelector("#order_extras");
+  const orderName = document.querySelector("#order_name");
+
+  // Updating order details in the DOM
+  orderType.textContent = typeSelect.selectedOptions[0].text;
+  orderToppings.textContent = toppings.join(", ");
+  orderExtras.textContent = extras.join(", ");
+  orderName.textContent = customerName; // display name in order
+};
+
+form.addEventListener("change", pancakePriceCalc);
+orderButton.addEventListener("click", displayOrder);
